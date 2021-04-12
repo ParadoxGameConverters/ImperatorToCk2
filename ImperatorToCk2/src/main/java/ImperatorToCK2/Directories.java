bcbe209212e7d51900beec1613eb35dd5a974618
DrDudelsack <72721325+DrDudelsack@users.noreply.github.com>
2021-04-12 00:06:55 -0400

    Update Directories.java

--- ImperatorToCk2/src/main/java/ImperatorToCK2/Directories.java
+++ ImperatorToCk2/src/main/java/ImperatorToCK2/Directories.java
@@ -1,5 +1,5 @@
 package ImperatorToCK2;
-
+ 
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
@@ -104,6 +104,7 @@ public class Directories
         String VM = "\\";
         char VMq = '"';
         String VN = "//";
+        char VN2 = '/';
         VN = VN.substring(0);
         VM = VM.substring(0);
         int aqq = 1;
@@ -115,7 +116,7 @@ public class Directories
 
         out.println("name="+VMq+modName+VMq);
 
-        out.println("path="+VMq+"mod"+VN+modName+VMq);
+        out.println("path="+VMq+"mod"+VN2+modName+VMq);
 
         out.println("user_dir="+VMq+modName+VMq);
 
