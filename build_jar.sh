d8c0495b0a58f1bd3a225d1e66a53cf7ec8483d4
Idhrendur <idhrendur@hotmail.com>
2021-06-14 17:30:33 -0700

    Simplify jar build script

--- build_jar.sh
+++ build_jar.sh
@@ -1,4 +1,3 @@
-msbuild -m ImperatorToCK2.sln /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 cd ImperatorToCk2
 mvn package
 cp target/ImperatorToCK2.jar ../Release/ImperatorToCK2
