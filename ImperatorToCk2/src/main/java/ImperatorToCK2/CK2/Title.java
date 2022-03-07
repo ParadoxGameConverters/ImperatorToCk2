4a4bc96c9edbe132977565c012383b229f00668e
Idhrendur <idhrendur@hotmail.com>
2022-03-06 17:46:52 -0800

    Update color in title

--- ImperatorToCk2/src/main/java/ImperatorToCK2/CK2/Title.java
+++ ImperatorToCk2/src/main/java/ImperatorToCK2/CK2/Title.java
@@ -1,9 +1,17 @@
 package ImperatorToCK2.CK2;
 
+import java.util.Optional;
+
 public class Title {
-    public Title(String imperatorTag, String irColor, String government, String capital, String rank) {
+    public Title(String imperatorTag, String imperatorColor, String government, String capital, String rank) {
         this.name = rank +"_" + imperatorTag;
-        this.irColor = irColor;
+
+        if (imperatorColor.equals("none")) {
+            this.color = Optional.empty();
+        } else {
+            this.color = Optional.of(imperatorColor);
+        }
+
         this.government = government;
         this.capital = capital;
         this.rank = rank;
@@ -14,7 +22,7 @@ public class Title {
     }
 
     private final String name;
-    public final String irColor;
+    public final Optional<String> color;
     public final String government;
     public final String capital;
     public final String rank;
