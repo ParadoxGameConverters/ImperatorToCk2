83a9469f820f9c08ef06f668e8f22bf33d9ac92c
Idhrendur <idhrendur@hotmail.com>
2022-03-06 17:01:07 -0800

    Pass Title to the function

--- ImperatorToCk2/src/main/java/ImperatorToCK2/CK2/Title.java
+++ ImperatorToCk2/src/main/java/ImperatorToCK2/CK2/Title.java
@@ -1,6 +1,14 @@
 package ImperatorToCK2.CK2;
 
 public class Title {
+    public Title(String irTAG, String irColor, String government, String capital, String rank) {
+        this.irTAG = irTAG;
+        this.irColor = irColor;
+        this.government = government;
+        this.capital = capital;
+        this.rank = rank;
+    }
+
     public String irTAG;
     public String irColor;
     public String government;
