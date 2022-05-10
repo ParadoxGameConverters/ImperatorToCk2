2fdcbff9549a23b313b2428d8d75344c2103e873
DrDudelsack <72721325+DrDudelsack@users.noreply.github.com>
2022-05-10 01:36:44 -0400

    Readme and License

--- ImperatorToCk2/Copy_Files.bat
+++ ImperatorToCk2/Copy_Files.bat
@@ -3,4 +3,8 @@ del "..\Release\ImperatorToCK2\defaultOutput" /Q /S /F
 rmdir "..\Release\ImperatorToCK2\defaultOutput" /Q /S
 xcopy "DataFiles\defaultOutput" "..\Release\ImperatorToCK2\defaultOutput" /Y /E /I
 
+mkdir "..\Release\Docs"
+copy "Data_Files\Readme.txt" "..\Release\Docs\"
+copy "Data_Files\license.txt" "..\Release\Docs\"
+
 git rev-parse HEAD > ..\Release\commit_id.txt
