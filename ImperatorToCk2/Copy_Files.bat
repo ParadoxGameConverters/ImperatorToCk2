d54e935ef9995eff5218d160308ee3e84b820c60
DrDudelsack <72721325+DrDudelsack@users.noreply.github.com>
2022-05-10 02:24:35 -0400

    Update Copy_Files.bat

--- ImperatorToCk2/Copy_Files.bat
+++ ImperatorToCk2/Copy_Files.bat
@@ -3,7 +3,7 @@ del "..\Release\ImperatorToCK2\defaultOutput" /Q /S /F
 rmdir "..\Release\ImperatorToCK2\defaultOutput" /Q /S
 xcopy "DataFiles\defaultOutput" "..\Release\ImperatorToCK2\defaultOutput" /Y /E /I
 
-copy "Data_Files\Readme.txt" "..\Release\ImperatorToCK2"
-copy "Data_Files\license.txt" "..\Release\ImperatorToCK2"
+copy "DataFiles\Readme.txt" "..\Release\ImperatorToCK2"
+copy "DataFiles\license.txt" "..\Release\ImperatorToCK2"
 
 git rev-parse HEAD > ..\Release\commit_id.txt
